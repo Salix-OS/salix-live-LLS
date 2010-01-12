b24c234faa6d0a83d1e2cc2edc50b805223ee0a2
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-01-12 00:21:08 +0000

    FIX: Translations in create-slxsave-xfs. German and Greek translations must be reviewed.

--- liveenv/create_txz.sh
+++ liveenv/create_txz.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ $UID -ne 0 ]; then
+  echo "you need to be root"
+  exit 1
+fi
 cd $(dirname $0)
 VER=$(cat version)
 RLZ=$(cat release)
@@ -7,6 +11,8 @@ HERE=$PWD
 cp -ar root/* $T/
 cd $T
 find . -type d -name '.svn' | xargs -i@ rm -rf @
+chown -R 0:0 .
+chown -R 1000:100 home/*
 /sbin/makepkg -l y -c n $HERE/../liveenv-$VER-noarch-$RLZ.txz
 cd $HERE
 rm -rf $T

b63a0448b449b662a121b33ada59d2ad0b8a1f6d
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-01-12 23:16:35 +0000

    FIX: Unix rights int the liveenv package creation. ADD: Japanese translation in the create-xlssave-xfs desktop files. ADD: lilosetup, salix-live-installer and create-slxsave-xfs launchers on the "one"'s Desktop.

--- liveenv/create_txz.sh
+++ liveenv/create_txz.sh
@@ -13,6 +13,11 @@ cd $T
 find . -type d -name '.svn' | xargs -i@ rm -rf @
 chown -R 0:0 .
 chown -R 1000:100 home/*
+chown -R 0:100 etc/ssh
+chmod u=rwx,g=rs,o=rw etc/ssh
+chmod go-r etc/ssh/*_key
+chown :43 etc/shadow
+chmod +x etc/profile.d/* etc/rc.d/* home/one/.xinitrc usr/libexec/* usr/sbin/*
 /sbin/makepkg -l y -c n $HERE/../liveenv-$VER-noarch-$RLZ.txz
 cd $HERE
 rm -rf $T
---
+++ liveenv/src-create-slxsave-xfs/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd $(dirname $0)
+root=../root
+cp *.desktop $root/usr/share/applications/
+cp create-slxsave-xfs.desktop $root/home/one/Desktop/
+for po in *.po; do
+  l=$(echo $po|sed 's/create-slxsave-xfs-\(.*\)\.po/\1/')
+  if [ ! -e $root/usr/share/locale/$l/LC_MESSAGES ]; then
+    mkdir -p $root/usr/share/locale/$l/LC_MESSAGES
+  fi
+  msgfmt $po -o $root/usr/share/locale/$l/LC_MESSAGES/create-slxsave-xfs.mo
+done
